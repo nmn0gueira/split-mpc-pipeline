c32fb56eeb929d75ec9350c89e8b5dfa354df173
Nuno Nogueira <101476982+nmn0gueira@users.noreply.github.com>
2026-03-23 21:17:18 +0000

    Update run.sh for flexible execution

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,16 +1,32 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
-protocol=$1
+executable=$1
 MP_SPDZ_PATH="MP-SPDZ"
 
 usage() {
+    echo "Script to run MP-SPDZ executables"
+    echo "Example usages:"
     echo "Usage 1: $0 <protocol_script> <program_name> [<runtime_args>]"
     echo "Usage 2: $0 <protocol_binary> <party> <program_name> [<runtime_args>]"
 }
 
-if [ -z "$protocol" ]; then
+while getopts ":h" opt; do
+    case $opt in
+        h)
+            usage
+            exit 1
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+if [ -z "$executable" ]; then
     usage
     exit 1
 fi
@@ -19,16 +35,13 @@ shift
 
 cd $MP_SPDZ_PATH
 
-if [[ "$protocol" == *.sh ]]; then   # If a protocol script is specified we execute it as such (which in turn executes it in localhost)
-    if [ -z "$1" ]; then
-        usage
-        exit 1
-    fi
-    Scripts/${protocol} "$@"
+if [[ "$executable" == *.sh ]]; then
+    Scripts/"${executable}" "$@"
+
+elif [[ "$executable" == *.x ]]; then
+    ./"${executable}" "$@"
 else
-    if [ -z "$1" ] || [ -z "$2" ]; then
-        usage
-        exit 1
-    fi
-    ./$protocol "$@"
+    echo "Unsupported executable format"
+    usage
+    exit 1
 fi
